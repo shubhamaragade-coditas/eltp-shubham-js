72b79860cc6caa10691747ae47eb68aa65814616
shubhamaragade-coditas <158129280+shubhamaragade-coditas@users.noreply.github.com>
2024-01-31 12:53:52 +0530

    Add files via upload

---
+++ assignment-day-three.js
@@ -0,0 +1,22 @@
+// Create a file and declare variables and assign string, boolean, undefined and null data types
+const stringVariable = "This is String";
+const booleanVariable = true;
+const undefinedVariable = undefined;
+const nullVariable = null;
+
+// Create file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+const numVariable = 1331;
+const arrayDatatype = [2,3,5,6,];
+const objectDatatype = {
+    animals : ["Dog","Cat"],
+    evenNumbers : [2,4,6,8],
+    year : 2024
+}
+
+console.log(typeof numVariable);  // number
+console.log(typeof stringVariable); // string
+console.log(typeof booleanVariable);  // boolean
+console.log(typeof undefinedVariable); // undefined
+console.log(typeof nullVariable); // object
+console.log(typeof arrayDatatype); // object
+console.log(typeof objectDatatype); // object
